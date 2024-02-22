df56e81dc85f35bafd5bbfaf9e6810445264f9a1
Ryan Ramola <50272454+rramola@users.noreply.github.com>
2024-02-22 01:13:14 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const container = document.getElementById("container");
+const dropdowns = document.getElementById("dropdowns");
+const targetDiv = document.getElementById("targetDiv");
+const elements = document.getElementById("element");
+const afterBeginBeforeEnd = document.getElementById("afterBeginBeforeEnd");
+const removalSelectorDiv = document.getElementById("removalSelectorDiv");
+const removalSelector = document.getElementById("removalSelector");
+const div1 = document.getElementById("div-1");
+const div2 = document.getElementById("div-2");
+const div3 = document.getElementById("div-3");
+const div4 = document.getElementById("div-4");
+
+targetDiv.addEventListener("click", function() {
+    let x = targetDiv.value;
+    console.log(x)
+});
+
+
+function getDiv() {
+
+}
+// console.log(container)
+// console.log(dropdowns)
+// console.log(targetDiv)
+// console.log(elements)
+// console.log(afterBeginBeforeEnd)
+// console.log(removalSelectorDiv)
+// console.log(removalSelector)
+// console.log(div1)
+// console.log(div2)
+// console.log(div3)
+// console.log(div4)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# DOM Practice 2:
+
+Another small practice to hold y'all over for now. Use the "Choose Elements destination" selector to choose which destination div your element will be added to (div-1, div-2, div-3, or div-4). Use "Choose an element" to decide which element/tag type the created element will be. Use "Text to be displayed" input to add the text to the element. Use "afterbegin or beforeend" selector to choose the placement of the element inside of said destination. Only hint, insertAdjacentHtml.
+
+Extra layer, use the "Item key to be removed" selector and the "remove" button to be able to remove specific elements that you have added. Don't forget to populate the drop box!
